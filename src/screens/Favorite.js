18c30caba6a51d0843c54179ac21e07e963c40b4
PinChangChung <130741403+PinChangChung@users.noreply.github.com>
2024-04-01 09:23:17 +0800

    Fix search bar

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -16,19 +16,20 @@ const Favorite = () => {
   return (
     <ScrollView style={{ flex: 1, backgroundColor: "#FFE27B", height: "100%" }} >
       <Center>
-
-        <View style={styles.searchbar} >
-          <View className="flex-row justify-end items-center rounded-full" style={{backgroundColor:'light gray'}}>
-            <TextInput
-              placeholder="搜尋站名" 
-              placeholderTextColor={'#F29D38'}
-              style={styles.searchtext}
+        <Box style={styles.searchbar} >
+          <View className="rounded-full" style={{ backgroundColor: 'light gray' }}>
+            <Box h={"100%"} justifyContent="center" >
+              <TextInput
+                placeholder="搜尋站名"
+                placeholderTextColor={'#F29D38'}
+                style={styles.searchtext}
               />
               <TouchableOpacity style={styles.magnify} onPress={() => null}>
-              <MaterialCommunityIcons name="magnify" size={55} color={"#F29D38"} />
+                <MaterialCommunityIcons name="magnify" size={55} color={"#F29D38"} />
               </TouchableOpacity>
+            </Box>
           </View>
-        </View>
+        </Box>
 
         <VStack>
           <HStack>
@@ -37,7 +38,7 @@ const Favorite = () => {
                 <HStack h={100} justifyContent="center" alignItems="center" mt={50}>
                   <Box bg="#D9D9D9" h={150} w={200}>
                     <Center h={"100%"}>
-                      <Text style={{fontSize:72}}>
+                      <Text style={{ fontSize: 72 }}>
                         MAP
                       </Text>
                     </Center>
@@ -46,7 +47,7 @@ const Favorite = () => {
               </Box>
             </Center>
           </HStack>
-          <HStack w={"100%"} h={125} space="lg" justifyContent="center" marginTop={22}> 
+          <HStack w={"100%"} h={125} space="lg" justifyContent="center" marginTop={22}>
             <Box w={"80%"} h={"70%"} bg="#fff" borderRadius={20} style={styles.shadow}>
               <TouchableOpacity onPress={() => navigate("Near")}>
                 <VStack h={"100%"} justifyContent="center" >
@@ -59,7 +60,7 @@ const Favorite = () => {
             <Box w={"80%"} h={"70%"} bg="#fff" borderRadius={20} style={styles.shadow}>
               <TouchableOpacity onPress={() => navigate("Near")}>
                 <VStack h={"100%"} justifyContent="center" >
-                <MaterialCommunityIcons name="heart" size={55} color={"red"} />
+                  <MaterialCommunityIcons name="heart" size={55} color={"red"} />
                 </VStack>
               </TouchableOpacity>
             </Box>
@@ -68,7 +69,7 @@ const Favorite = () => {
             <Box w={"80%"} h={"70%"} bg="#fff" borderRadius={20} style={styles.shadow}>
               <TouchableOpacity onPress={() => navigate("Near")}>
                 <VStack h={"100%"} justifyContent="center" >
-                <MaterialCommunityIcons name="heart" size={55} color={"red"} />
+                  <MaterialCommunityIcons name="heart" size={55} color={"red"} />
                 </VStack>
               </TouchableOpacity>
             </Box>
@@ -77,7 +78,7 @@ const Favorite = () => {
             <Box w={"80%"} h={"70%"} bg="#fff" borderRadius={20} style={styles.shadow}>
               <TouchableOpacity onPress={() => navigate("Near")}>
                 <VStack h={"100%"} justifyContent="center" >
-                <MaterialCommunityIcons name="heart" size={55} color={"red"} />
+                  <MaterialCommunityIcons name="heart" size={55} color={"red"} />
                 </VStack>
               </TouchableOpacity>
             </Box>
@@ -86,7 +87,7 @@ const Favorite = () => {
             <Box w={"80%"} h={"70%"} bg="#fff" borderRadius={20} style={styles.shadow}>
               <TouchableOpacity onPress={() => navigate("Near")}>
                 <VStack h={"100%"} justifyContent="center">
-                <MaterialCommunityIcons name="heart" size={55} color={"red"} />
+                  <MaterialCommunityIcons name="heart" size={55} color={"red"} />
                 </VStack>
               </TouchableOpacity>
             </Box>
@@ -105,27 +106,27 @@ const styles = StyleSheet.create({
     shadowRadius: 1.5,
     elevation: 4,
   },
-  searchbar:{
-    height:'10%',
-    width:'80%',
-    backgroundColor:'#fff',
-    borderRadius:50,
-    marginBottom:20,
-    marginTop:10,
+  searchbar: {
+    height: 65,
+    width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    marginBottom: 20,
+    marginTop: 10,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 2,
     shadowRadius: 1.5,
     elevation: 4,
   },
-  searchtext:{
-    marginLeft:15,
-    marginTop:12,
-    fontSize:20,
+  searchtext: {
+    marginLeft: 15,
+    marginTop: 12,
+    fontSize: 20,
   },
-  magnify:{
-    marginTop:-40,
-    marginLeft:'80%'
+  magnify: {
+    marginTop: -40,
+    marginLeft: '80%'
   }
 })
 
