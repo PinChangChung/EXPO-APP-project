79cb71cdf4cfc89c892b440ebd3b32f9883016e9
Rayping <130742095+GLMrps@users.noreply.github.com>
2024-04-10 19:15:07 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getUbikeInfo = async () => {
   const { data } = await axios.get(
-    "https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json/preview"
+    "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
   );
   return data;
 };
