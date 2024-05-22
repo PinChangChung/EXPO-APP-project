ddc22ac7ced84a4b42615e008bc557867d05ed4e
Rayping <130742095+GLMrps@users.noreply.github.com>
2024-05-23 01:23:17 +0800

    Update Loading.js

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Navigation from "../navigation";
 import { Image, Center, Text } from "@gluestack-ui/themed";
-
+import { View, StyleSheet} from "react-native";
 import { useUbikeInfo } from '../tanstack-query';
 import iconImg from "../../midterm_img/icon-new.png"
+import bicycleImg from "../../midterm_img/Bicycle.png"
+import LottieView from "lottie-react-native";
 
 const Loading = () => {
     const { isLoading, isSuccess } = useUbikeInfo();
     const icon = iconImg;
+    const bicycle = bicycleImg;
+
+
+
 
     return (
         <>
@@ -19,12 +25,28 @@ const Loading = () => {
                         <Text mt={10} color="#fff">
                             正在加載站點資料...
                         </Text>
+                        <View style={styles.bike}>
+                        <LottieView style={{flex:1}} source={require("../json/bike.json")} speed={0.5} autoPlay loop/>
+                        </View>
                     </Center>
                     : <Navigation />
             }
         </>
     )
+
+    
 };
+
+const styles=StyleSheet.create({
+
+bike:{
+    height:100,
+    aspectRatio:3
+}
+
+
+})
+
 
 
 export default Loading;
